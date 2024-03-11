b819a4616d0e139b2e68e89fcf6ce0e45fe66786
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-11 19:12:20 +0100

    V2207: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: vivo Model: V2207 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_V2207.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_V2207-user \
+    omni_V2207-userdebug \
+    omni_V2207-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for vivo V2207 (V2207)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
